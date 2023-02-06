b9ff1b547eaec80a1f5ffe56453619e3470cd558
CiaoBambino <matteolani@live.fr>
2023-02-06 19:23:04 +0100

    definition de la classe

---
+++ models/tournament.py
@@ -0,0 +1,21 @@
+class Tournament:
+
+    actual_round = 1
+    all_tournament_list = []
+    rounds_list = []
+    registered_players = []
+
+
+    def __init__(self, name,
+                 place,
+                 starting_date=None,
+                 ending_date=None,
+                 description=None,
+                 number_of_rounds=4):
+        self.name = name
+        self.place = place
+        self.starting_date = starting_date
+        self.endingdate = ending_date
+        self.number_of_rounds = number_of_rounds
+        self.description = description
+        

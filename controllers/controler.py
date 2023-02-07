158ac696d242c0b88a4e4b9fce00bf21fbc6ead9
CiaoBambino <matteolani@live.fr>
2023-02-07 14:39:41 +0100

    changement mineur

--- controllers/controler.py
+++ controllers/controler.py
@@ -1,1 +1,2 @@
 class Controler:
+    pass
--- views/view.py
+++ views/view.py
@@ -1,2 +1,9 @@
+import curses
+
 class View:
-    pass
+    
+    def __init__(self) -> None:
+        
+        terminal = curses.initscr()
+
+        curses.endwin()

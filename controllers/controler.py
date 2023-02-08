f7d798a7a819afe80fb86f6a4bcec10731dd8c88
CiaoBambino <matteolani@live.fr>
2023-02-08 19:52:04 +0100

    ajout de prototypes de fonctions de base et création de serializer

--- controllers/controler.py
+++ controllers/controler.py
@@ -1,5 +1,41 @@
+import json
+import os
+
+
 class Controler:
-    
+
     def __init__(self, view):
-        
+
         self.view = view
+
+    def run(self):
+        pass
+
+    def synchroniser():
+        pass
+
+    def get_player():
+        pass
+
+    def add_player():
+        pass
+
+
+class JSONTranslate:
+
+    def serializer(Object):
+        name = Object.__name__ + ".json"
+        directory_name = ".\data"
+        complete_name = os.path.join(directory_name, name)
+
+        with open("complete_name", 'a') as f:
+            pass
+
+
+
+
+    def unserializer():
+        pass
+
+class CreateTournament:
+    pass

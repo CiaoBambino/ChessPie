2eabf149f542817862a9aff1319db9e50b5a8b36
CiaoBambino <matteolani@live.fr>
2023-02-08 14:31:46 +0100

    ajout du menu

--- views/view.py
+++ views/view.py
@@ -1,10 +1,10 @@
-import os
+from os import system, name
 
 class MainMenu:
 
     def __init__(self):
 
-        os.system('cls')
+        ClearTerminal()
         print("\n MENU PRINCIPAL \n")
         print("[1] TOURNOIS")
         print("[2] JOUEURS")
@@ -14,8 +14,31 @@ class MainMenu:
               "[n]nettoyer" + "      " +
               "[s]sauvegarder")
         
+        while True:
+            
+            user_choice = input("\n Entrez votre choix : ")
+            
+            match user_choice:
+                case "1":
+                    print("option1")
+                case "2":
+                    print("option2")
+                case "3":
+                    print("option3")
+                case "4":
+                    print("option4")
+                case "q" | "Q" | "quit" | "quitt" | "quitter" | "Quitter":
+                    exit()
+                case "n":
+                    print("option56")
+                case "s":
+                    print("otoion7")
+                case _:
+                    print("Cette option n'existe pas")
+                
     def __call__(self, *args, **kwargs):
         pass  
+    
 
 class TournamentMenu:
     pass
@@ -34,3 +57,24 @@ class RapportMenu:
 
 class SettingMenu:
     pass
+
+class ClearTerminal:
+    """Clear the terminal"""
+
+    def __call__(self):
+        
+        """
+        if (os.name == 'posix'):
+            os.system('clear')
+        # else screen will be cleared for windows
+        else:
+             os.system('cls')
+        """
+
+        # for windows
+        if name == 'nt':
+            _ = system('cls')
+        # others
+        else:
+            _ = system('clear')     
+       

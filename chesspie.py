9803b2cc68e688ff0c3880d6a96688a5c4cdaa3f
CiaoBambino <matteolani@live.fr>
2023-02-08 19:52:33 +0100

    PEP8

--- chesspie.py
+++ chesspie.py
@@ -1,12 +1,12 @@
 from views.view import MainMenu
 from controllers.controler import Controler
 
+
 def main():
-  
+
     view = MainMenu()
     chess = Controler(view)
-
-
+    chess.run()
 
 if __name__ == "__main__":
     main()

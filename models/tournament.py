cc429f05fe8425c4b98f7806758f405da6a02a36
CiaoBambino <matteolani@live.fr>
2023-02-10 20:29:41 +0100

    ajout d'un serializer/deserializer

--- models/tournament.py
+++ models/tournament.py
@@ -18,7 +18,7 @@ class Tournament:
         self.name = name
         self.place = place
         self.starting_date = starting_date
-        self.endingdate = ending_date
+        self.ending_date = ending_date
         self.description = description
         self.tournament_id = tournament_id
         self.registered_players = registered_players
@@ -26,5 +26,42 @@ class Tournament:
         self.rounds_list = rounds_list
         self.number_of_rounds = number_of_rounds
 
-        if tournament_id == None:
+        if tournament_id is None:
             tournament_id = Tournament.TOURNAMENT_ID
+
+    def serializer(self):
+
+        tournament_data = {}
+        tournament_data['name'] = self.name
+        tournament_data['place'] = self.place
+        tournament_data['starting_date'] = self.starting_date
+        tournament_data['ending_date'] = self.ending_date
+        tournament_data['description'] = self.description
+        tournament_data['tournament_id'] = self.tournament_id
+        tournament_data['actual_round'] = self.actual_round
+        tournament_data['rounds_list'] = self.rounds_list
+        tournament_data['number_of_rounds'] = self.number_of_rounds
+
+        return tournament_data
+
+    def unserializer(self, tournament_data):
+
+        name = tournament_data['name']
+        place = tournament_data['place']
+        starting_date = tournament_data['starting_date']
+        ending_date = tournament_data['ending_date']
+        description = tournament_data['description']
+        tournament_id = tournament_data['tournament_id']
+        actual_round = tournament_data['actual_round']
+        rounds_list = tournament_data['rounds_list']
+        number_of_rounds = tournament_data['number_of_rounds']
+
+        return Tournament(name,
+                          place,
+                          starting_date,
+                          ending_date,
+                          description,
+                          tournament_id,
+                          actual_round,
+                          rounds_list,
+                          number_of_rounds)

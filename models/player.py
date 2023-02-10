30ad7089d7b58a10fe6af0b70abd724d3b7728fa
CiaoBambino <matteolani@live.fr>
2023-02-10 14:08:38 +0100

    renommage class PLayer en Player + correction total player list

--- models/player.py
+++ models/player.py
@@ -1,5 +1,8 @@
-class PLayer:
+import time
 
+class Player:
+
+    CLASS_NAME = "player"
     TOTAL_PLAYER_NUMBER = 0
     TOTAL_PLAYER_LIST = []
     PLAYER_ID_INCREMENT = 0
@@ -7,13 +10,13 @@ class PLayer:
     def __init__(self, name,
                  first_name,
                  birthday,
+                 note,
                  player_id=None,
                  total_score=None,
-                 tournament_score=None,
-                 note=None):
+                 tournament_score=None):
 
-        self.PLAYER_ID_INCREMENT += 1
-        self.TOTAL_PLAYER_NUMBER += 1
+        Player.PLAYER_ID_INCREMENT += 1
+        Player.TOTAL_PLAYER_NUMBER += 1
         self.name = name
         self.first_name = first_name
         self.birthday = birthday
@@ -21,7 +24,14 @@ class PLayer:
         self.total_score = 0
         self.tournament_score = 0
         self.note = note
-        self.TOTAL_PLAYER_LIST.append(self.Player)
+        self.TOTAL_PLAYER_LIST.append(self)
+
+        if len(self.TOTAL_PLAYER_LIST) > 1:
+            print(self.TOTAL_PLAYER_LIST[1])
+            time.sleep(10)
+
+    def __call__(self):
+        return self.serializer()
     
     def serializer(self):
 
@@ -29,30 +39,30 @@ class PLayer:
         player_data['name'] = self.name
         player_data['first_name'] = self.first_name
         player_data['birthday'] = self.birthday
+        player_data['note'] = self.note
         player_data['player_id'] = self.player_id
         player_data['total_score'] = self.total_score
         player_data['tournament_score'] = self.tournament_score
-        player_data['note'] = self.note
-        
+
         return player_data
 
     def unserializer(self, player_data):
-        
+
         name = player_data['name']
         first_name = player_data['first_name']
         birthday = player_data['birthday']
+        note = player_data['note']
         player_id = player_data['player_id']
         total_score = player_data['total_score']
         tournament_score = player_data['tournament_score']
-        note = player_data['note']
-        
+
         return PLayer(name,
                       first_name,
                       birthday,
+                      note,
                       player_id,
                       total_score,
-                      tournament_score,
-                      note)
+                      tournament_score)
 
     def __str__(self) -> str:
         return self.first_name + " " + self.name + " " + self.player_id + " " + self.note

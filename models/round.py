27d484c145271db5df5e89047ca28b271e39afbc
CiaoBambino <matteolani@live.fr>
2023-02-13 20:25:12 +0100

    minor change

--- models/round.py
+++ models/round.py
@@ -6,11 +6,12 @@ class Round:
     def __init__(self, actual_round,
                  starting_time,
                  ending_time,
-                 note=None,
-                 match_list=None):
+                 match_list,
+                 note=None):
 
         self.actual_round = tournament.Tournament.actual_round
         self.name = "Round %s" % actual_round
         self.starting_time = starting_time
         self.ending_time = ending_time
         self.match_list = match_list
+        self.note = note

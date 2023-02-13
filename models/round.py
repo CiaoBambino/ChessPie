1b9340700cde9a327a8a370ebf4225f9f7e932f9
CiaoBambino <matteolani@live.fr>
2023-02-13 17:48:05 +0100

    changed variable init

--- models/round.py
+++ models/round.py
@@ -6,10 +6,11 @@ class Round:
     def __init__(self, actual_round,
                  starting_time,
                  ending_time,
-                 note=None):
+                 note=None,
+                 match_list=None):
 
         self.actual_round = tournament.Tournament.actual_round
         self.name = "Round %s" % actual_round
         self.starting_time = starting_time
         self.ending_time = ending_time
-        self.match_list = []
+        self.match_list = match_list

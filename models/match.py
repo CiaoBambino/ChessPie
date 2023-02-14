68816a517e02b1ec98a35ab4145249eeed92394a
CiaoBambino <matteolani@live.fr>
2023-02-14 13:05:57 +0100

    rewriting the class and __str__, __repr__

--- models/match.py
+++ models/match.py
@@ -2,13 +2,16 @@
 
 class Match:
 
-    def __init__(self, player_one, player_two):
-
-        self.player_one = player_one
-        self.player_two = player_two
-
+    def __init__(self, A, A_score, B, B_score,):
+        
+        self.A = A
+        self.A_score = A_score
+        self.B = B
+        self.B_score = B_score
+        self.matche = ([A, A_score], [B, B_score])
+        
     def __str__(self) -> str:
-        return "Match opposant " + self.player_one + " à " + self.player_two
+        return "Match opposant " + self.A['first_name'] + " à " + self.B['first_name']
 
     def __repr__(self) -> str:
-        return "Match(player 1='{}', player 2='{}')".format(self.player_one, self.player_two)
+        return "Match(player 1='{}', player 2='{}')".format(self.A['name'], self.B['name'])

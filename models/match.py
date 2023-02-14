b60cd2337604b12eeb7635fb3bf39c65c3c4c648
CiaoBambino <matteolani@live.fr>
2023-02-14 14:13:11 +0100

    rewriting class Match to accept list of Tuple

--- models/match.py
+++ models/match.py
@@ -3,13 +3,13 @@
 class Match:
 
     def __init__(self, A, A_score, B, B_score,):
-        
+
         self.A = A
         self.A_score = A_score
         self.B = B
         self.B_score = B_score
         self.matche = ([A, A_score], [B, B_score])
-        
+
     def __str__(self) -> str:
         return "Match opposant " + self.A['first_name'] + " à " + self.B['first_name']
 

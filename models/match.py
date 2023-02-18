e28da98b2b85d8be42905f432d27259b8c593feb
CiaoBambino <matteolani@live.fr>
2023-02-18 16:27:02 +0100

    add getitem and setitem magic method

--- models/match.py
+++ models/match.py
@@ -1,5 +1,3 @@
-
-
 class Match:
 
     def __init__(self, A, A_score, B, B_score, color):
@@ -15,4 +13,10 @@ class Match:
         return "Match opposant " + self.A['first_name'] + " ({color})".format(color=self.color) + " à " + self.B['first_name']
 
     def __repr__(self) -> str:
-        return "Match(player 1='{}', player 2='{}')".format(self.A['name'], self.B['name'])
+        return "Match([{},{}], [{},{}])".format(self.A['name'], self.A_score, self.B['name'], self.B_score)
+
+    def __getitem__(self, index):
+        return self.matche[index]
+
+    def __setitem__(self, key, value):
+        self.matche[key] = value
